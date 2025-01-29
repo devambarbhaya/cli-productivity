1979fca0cab8c69e0a520b9135cc8d37310808ab
devambarbhaya <barbhaya.devam@gmail.com>
2025-01-29 05:47:27 +0000

    reconfigure the base declaration

--- database/__init__.py
+++ database/__init__.py
@@ -1,6 +1,5 @@
 from sqlalchemy import create_engine, Column, Integer, String, Text, DateTime
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 import datetime
 import os
 
