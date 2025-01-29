0fdc72005dbd502021f1010b150681879f335183
devambarbhaya <barbhaya.devam@gmail.com>
2025-01-29 05:55:30 +0000

    add 'delete task' functionality

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -1,7 +1,7 @@
 from database import SessionLocal, Task
 from sqlalchemy.exc import SQLAlchemyError
 
-def add_tasks(title, description, priority, tags, deadline):
+def add_task(title, description, priority, tags, deadline):
     session = SessionLocal()
     try:
         task = Task(
@@ -25,3 +25,14 @@ def list_tasks():
     tasks = session.query(Task).all()
     session.close()
     return tasks
+
+def delete_task(task_id):
+    session = SessionLocal()
+    task = session.query(Task).filter(Task.id == task_id).first()
+    if task:
+        session.delete(task)
+        session.close()
+        print(f"Task ID {task_id} deleted.")
+    else:
+        print("Task not found.")
+    session.close()
