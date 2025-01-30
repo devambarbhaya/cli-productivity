4b5bb95beae3c2aa07b2326ebd8bf6787b89d98b
devambarbhaya <barbhaya.devam@gmail.com>
2025-01-30 03:30:18 +0000

    package and distribute

--- setup.py
+++ setup.py
@@ -1,17 +1,28 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="task-cli",
-    version="1.0",
+    name="my_project",
+    version="1.0.0",
     packages=find_packages(),
+    include_package_data=True,
     install_requires=[
-        "click",
-        "sqlalchemy",
-        "plyer"
+        "Click",
+        "SQLAlchemy",
+        "plyer", # Required for Linux notifications
     ],
     entry_points={
         "console_scripts": [
-            "task=cli.__init__:cli",
+            "task-cli=cli.__init__:cli",
         ],
     },
+    author="Devam Barbhaya",
+    description="A CLI-based task management tool.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
 )
